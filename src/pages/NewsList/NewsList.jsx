0cf94bb620b19c757931ac0e7bbe7576591e3251
jaquelineportela <jaquelineportelan@gmail.com>
2025-02-22 15:43:24 -0300

    ajuste newsList

--- src/pages/NewsList/NewsList.jsx
+++ src/pages/NewsList/NewsList.jsx
@@ -1,10 +1,9 @@
-// src/pages/NewsList.js
+
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './NewsList.css';
 import Button from '../../components/Button/Button'
-
 
 const NewsList = () => {
 
@@ -27,35 +26,34 @@ const NewsList = () => {
       });
   }, []);
   
-  if (loading) {
-    return <div className="loading">Carregando notícias...</div>;
-  }
-
-  if (newsList.length === 0) {
-    return <div className="no-news">Nenhuma notícia encontrada.</div>;
-  }
-
-  const displayedNews = newsList.slice(0, 4);
-
   return (
     <section id='news-id'>
       <div className="news-list">
         <h1 className='title-n'>Últimas Notícias</h1>
-        <ul className='list-content'>
-          {displayedNews.map((news) => (
-            <li key={news.id}>
-              <Link to={`/noticia/${news.id}`}>
-                <img src={news.imagem} alt={news.titulo} className="news-image" />
-                <h3>{news.titulo}</h3>
-                <p className="meta">{new Date(news.dataCriacao).toLocaleDateString()}</p>
-                <p>{news.conteudo.substring(0, 100)}...</p> 
-              </Link> 
-            </li>
-          ))}
-        </ul>
-        <div className='btn-right'>
-          <Button text="Mais notícias" color="green" onClick={() => navigate('/noticias')} />
-        </div>
+
+        {loading ? (
+          <div className="loading">Carregando notícias...</div>
+        ) : newsList.length === 0 ? (
+          <div className="no-news">Nenhuma notícia encontrada.</div>
+        ) : (
+          <>
+            <ul className='list-content'>
+              {newsList.slice(0, 4).map((news) => (
+                <li key={news.id}>
+                  <Link to={`/noticia/${news.id}`}>
+                    <img src={news.imagem} alt={news.titulo} className="news-image" />
+                    <h3>{news.titulo}</h3>
+                    <p className="meta">{new Date(news.dataCriacao).toLocaleDateString()}</p>
+                    <p>{news.conteudo.substring(0, 200)}...</p> 
+                  </Link> 
+                </li>
+              ))}
+            </ul>
+            <div className='btn-right'>
+              <Button text="Mais notícias" color="green" onClick={() => navigate('/noticias')} />
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
